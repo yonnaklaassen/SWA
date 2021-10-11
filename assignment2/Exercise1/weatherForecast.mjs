09a4e432ae1dfcad4db7252523fadad60a8bca04
Rafal <rafalpierscieniak20@gmail.com>
2021-10-11 21:03:14 +0200

    weather forecast finished

--- assignment2/Exercise1/weatherForecast.mjs
+++ assignment2/Exercise1/weatherForecast.mjs
@@ -4,11 +4,11 @@ export function WeatherForecast(data) {
     const weather = Weather(data)
 
     const averageFromValue = () => {
-
+        return data.reduce((a, b) => a.min+b.min, 0)/data.length
     }
 
     const averageToValue = () => {
-
+        return data.reduce((a, b) => a.max+b.max,0)/data.length
     }
 
     return {...weather, averageFromValue, averageToValue}
