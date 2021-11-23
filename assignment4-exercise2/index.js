ea17e01e007647f23668f1082884e7048deb9d12
Rafal <rafalpierscieniak20@gmail.com>
2021-11-23 11:25:40 +0100

    initail stage - connection to the server

---
+++ assignment4-exercise2/index.js
@@ -0,0 +1,9 @@
+// const WebSocket = require('ws')
+//
+// const wss = new WebSocket.Server({port: 9095,path: "warnings"})
+//
+// const clients = {}
+//
+// wss.on('connection',(ws, req) =>{
+//     ws.on()
+// })
