3e419bee3d5d27c8474a6b72b357d8685e8c688f
Rafal <rafalpierscieniak20@gmail.com>
2021-11-26 23:21:52 +0100

    min-severity works

--- assignment4-exercise2/index.js
+++ assignment4-exercise2/index.js
@@ -1,9 +1,28 @@
-// const WebSocket = require('ws')
-//
-// const wss = new WebSocket.Server({port: 9095,path: "warnings"})
-//
-// const clients = {}
-//
-// wss.on('connection',(ws, req) =>{
-//     ws.on()
-// })
+export function Warnings() {
+
+    const retrieveData = (warning) => {
+        let element = document.getElementById(`${warning.id}`)
+
+        if (element === null) {
+            addWarning(warning)
+        } else {
+            updateWarning(element, warning)
+        }
+    }
+
+    const addWarning = (text) => {
+        const li = document.createElement('li')
+        li.setAttribute("id", `${text.id}`)
+        document.querySelector('#warnings').appendChild(li)
+        updateWarning(li, text)
+    }
+    const updateWarning = (element, newData) => {
+        const a = document.createElement('a')
+        a.innerText = JSON.stringify(newData)
+        element.appendChild(a)
+    }
+
+    return {
+        retrieveData, changeMinSeverity
+    }
+}
